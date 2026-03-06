90fd73b61404a5e1c63606c33a9cea9a51f15490
whypikmi <ttelatnikova1@yandex.ru>
2026-03-06 14:21:34 +0300

    Step 7 arrays

---
+++ code/Step7_ElementMassiv/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Вывод элементов массива");
+int[] array = { 10, 20, 30, 40, 50 };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ code/Step7_MaxMassiv/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Поиск максимального элемента");
+Console.Write("Введите количество элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[numbers];
+Console.Write($"Введите {numbers} элементов массива: ");
+for (int i = 0; i < numbers; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+int max = array[0];
+for (int i = 1; i < numbers; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"Максимальный элемент массива: {max}");

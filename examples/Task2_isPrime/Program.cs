15de21cb816b54034573225180bbfd48c0d6e807
ElizavetaTelyatnikova <ttelatnikova1@yandex.ru>
2026-03-10 20:59:29 +0300

    Task2 prime number

---
+++ examples/Task2_isPrime/Program.cs
@@ -0,0 +1,24 @@
+﻿bool isPrime(double a)
+{
+    if (a <=1)
+    {
+        return false;
+    }
+    for(double i = 2; i<a;i++)
+    {
+        if (a%i == 0)
+        return false;
+    }
+    return true;
+}
+Console.WriteLine("Нахождение простого числа");
+Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+if (isPrime(number))
+{
+    Console.WriteLine("Число простое");
+}
+else
+{
+    Console.WriteLine("Число не простое");
+}
